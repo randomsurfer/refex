3d946de0c39ed421cf918ed8ee2d4c40a0e4acc7
randomsurfer <randomsurfer@users.noreply.github.com>
2015-04-20 13:19:45 +0530

    1. Diversity constraint added. 2. Refactoring. 3. Updating requirements.

--- src/glrd.py
+++ src/glrd.py
@@ -31,19 +31,6 @@ G(.)(i) - denotes the i^th column vector of G.
 F(i)(.) - denotes the i^th row vector of F.
 '''
 
-# Sample data.
-m = 30
-n = 20
-r = 4
-
-np.random.seed(1)
-V = np.random.random_integers(1, 10, (m, n))
-
-fctr = nimfa.mf(V, rank=r, method="lsnmf", max_iter=100)
-fctr_res = nimfa.mf_run(fctr)
-G = np.asarray(fctr_res.basis())
-F = np.asarray(fctr_res.coef())
-
 # numpy 2d array slicing:
 # test = numpy.array([[1, 2], [3, 4], [5, 6]])
 # test[:,:] => full array
@@ -51,32 +38,100 @@ F = np.asarray(fctr_res.coef())
 # test[:,0] => 1st col
 # test[:,1] => 2nd col
 
-for k in xrange(r):
-    G_copy = np.copy(G)  # create local copies for excluding the k^th col and row of G and F resp.
-    F_copy = np.copy(F)
-    G_copy[:, k] = 0.0
-    F_copy[k, :] = 0.0
 
-    R = V - np.dot(G_copy, F_copy)  # compute residual
+def glrd_sparse(V, G, F, r, err_V, err_F):
+    for k in xrange(r):
+        G_copy = np.copy(G)  # create local copies for excluding the k^th col and row of G and F resp.
+        F_copy = np.copy(F)
+        G_copy[:, k] = 0.0
+        F_copy[k, :] = 0.0
 
-    # Solve for optimal G(.)(k)
-    F_k = F[k, :]
-    x_star = linalg.lstsq(R.T, F_k.T)[0].T
-    x = cvx.Variable(x_star.shape[0])
-    objective = cvx.Minimize(cvx.norm2(x_star - x))
-    constraints = [cvx.norm1(x) <= 0.5, x >= 0.0]
-    prob = cvx.Problem(objective, constraints)
-    result = prob.solve()
-    G_k_min = np.asarray(x.value)
-    G[:, k] = G_k_min[:, 0]
+        R = V - np.dot(G_copy, F_copy)  # compute residual
 
-    # Solve for optimal F(k)(.)
-    G_k = G[:, k]
-    x_star = linalg.lstsq(R, G_k)[0]
-    x = cvx.Variable(x_star.shape[0])
-    objective = cvx.Minimize(cvx.norm2(x_star - x))
-    constraints = [cvx.norm1(x) <= 0.5, x >= 0.0]
-    prob = cvx.Problem(objective, constraints)
-    result = prob.solve()
-    F_k_min = np.asarray(x.value)
-    F[k, :] = F_k_min[0, :]
+        # Solve for optimal G(.)(k) with sparsity constraints
+        F_k = F[k, :]
+        x_star_G = linalg.lstsq(R.T, F_k.T)[0].T
+        x_G = cvx.Variable(x_star_G.shape[0])
+        objective_G = cvx.Minimize(cvx.norm2(x_star_G - x_G))
+        constraints_G = [cvx.norm1(x_G) <= err_V, x_G >= 0.0]
+        prob_G = cvx.Problem(objective_G, constraints_G)
+        prob_G.solve()
+        G_k_min = np.asarray(x_G.value)
+        G[:, k] = G_k_min[:, 0]
+
+        # Solve for optimal F(k)(.) with sparsity constraints
+        G_k = G[:, k]
+        x_star_F = linalg.lstsq(R, G_k)[0]
+        x_F = cvx.Variable(x_star_F.shape[0])
+        objective_F = cvx.Minimize(cvx.norm2(x_star_F - x_F))
+        constraints_F = [cvx.norm1(x_F) <= err_F, x_F >= 0.0]
+        prob_F = cvx.Problem(objective_F, constraints_F)
+        prob_F.solve()
+        F_k_min = np.asarray(x_F.value)
+        F[k, :] = F_k_min[0, :]
+
+    return G, F
+
+
+def glrd_diverse(V, G, F, r, err_V, err_F):
+    for k in xrange(r):
+        G_copy = np.copy(G)  # create local copies for excluding the k^th col and row of G and F resp.
+        F_copy = np.copy(F)
+        G_copy[:, k] = 0.0
+        F_copy[k, :] = 0.0
+
+        R = V - np.dot(G_copy, F_copy)  # compute residual
+
+        # Solve for optimal G(.)(k) with diversity constraints
+        F_k = F[k, :]
+        x_star_G = linalg.lstsq(R.T, F_k.T)[0].T
+        x_G = cvx.Variable(x_star_G.shape[0])
+
+        objective_G = cvx.Minimize(cvx.norm2(x_star_G - x_G))
+
+        constraints_G = [x_G >= 0.0]
+        for j in xrange(r):
+            if j != k:
+                constraints_G += [x_G.T * G[:, j] <= err_V]
+
+        prob_G = cvx.Problem(objective_G, constraints_G)
+        prob_G.solve()
+        G_k_min = np.asarray(x_G.value)
+        G[:, k] = G_k_min[:, 0]
+
+        # Solve for optimal F(k)(.) with diversity constraints
+        G_k = G[:, k]
+        x_star_F = linalg.lstsq(R, G_k)[0]
+        x_F = cvx.Variable(x_star_F.shape[0])
+        objective_F = cvx.Minimize(cvx.norm2(x_star_F - x_F))
+
+        constraints_F = [x_F >= 0.0]
+        for j in xrange(r):
+            if j != k:
+                constraints_F += [x_F.T * F[j, :] <= err_F]
+
+        prob_F = cvx.Problem(objective_F, constraints_F)
+        prob_F.solve()
+        F_k_min = np.asarray(x_F.value)
+        F[k, :] = F_k_min[0, :]
+
+    return G, F
+
+
+if __name__ == "__main__":
+    # Sample data.
+    m = 30
+    n = 20
+    r = 4
+
+    np.random.seed(1)
+    V = np.random.random_integers(1, 10, (m, n))
+
+    fctr = nimfa.mf(V, rank=r, method="lsnmf", max_iter=100)
+    fctr_res = nimfa.mf_run(fctr)
+    G = np.asarray(fctr_res.basis())
+    F = np.asarray(fctr_res.coef())
+
+    G_, F_ = glrd_diverse(V, G, F, r, 0.5, 0.5)
+    print G_
+    print F_
